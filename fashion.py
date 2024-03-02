910ec3570bd0fb4e9695a06654f9f3e809ba88d6
Dhruv Kanetkar <kanetkard@gmail.com>
2024-03-02 09:33:32 -0800

    tester for clothing description gen

---
+++ fashion.py
@@ -0,0 +1,9 @@
+#create a function that takes in a link to an image 
+#should feed that image into gpt4-v
+#gets a text output of the image with the specific items - bucket into pants, shorts, hats, shirts, t-shirt, shoes, jackets, 
+#for each item get a description of the color, pattern, material, 
+#make a fetch to a database or json structure that has the matching combo of all three of these values
+#return the image links to be rendered on the front end
+
+
+
