9503e44d68a215ef8afacf88f8a0734c9250a1f0
Justin Chou <87676813+nakschou@users.noreply.github.com>
2024-03-01 20:57:04 -0800

    new test function

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
-app = Flask(__name__)
+import dspy
+import os
+from dotenv import load_dotenv
 
-@app.route('/')
+app = Flask(__name__)
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+turbo = dspy.OpenAI(model='gpt-4', api_key=OPENAI_API_KEY)
+dspy.configure(lm=turbo)
+
+@app.route('/test')
 def hello_world():
-    return 'Checking that this works!'
+    class SystemOfEquations(dspy.Signature):
+        """Solve for x and y in the system of equations"""
+
+        equations = dspy.InputField(desc="Comma-separated list of equations")
+        answer = dspy.OutputField(desc="JSON containing x and y")
+    qa = dspy.ChainOfThought(SystemOfEquations)
+    answer = qa(equations="3x+3y=42,2x-2y=0")
+    return answer.answer
+    
